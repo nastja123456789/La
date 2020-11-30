3fa92eb54b4b097971f3e91b591cf05e3dbf7ea2
nastja123456789 <60920907+nastja123456789@users.noreply.github.com>
2020-11-30 21:42:54 +0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+
+import VueRouter from 'vue-router'
+
+import routes from './routes'
+
+import Master from './components/layouts/Master'
+
+
+
+Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+/* eslint-disable no-new */
+
+
+new Vue({
+ router: router,
+  components: { Master },
+  template: '<Master/>'
+}).$mount('#app')
